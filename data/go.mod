1e11bae4ff11044a42530686307d4da025ee6176
Bregeras, Olivier <Olivier.Bregeras@sc.com>
2019-04-29 11:54:18 +0800

    Add watching filesystem and Makefile

--- data/go.mod
+++ data/go.mod
@@ -7,5 +7,5 @@ require (
 	firebase.google.com/go v3.7.0+incompatible
 	golang.org/x/net v0.0.0-20190311183353-d8887717615a
 	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a // indirect
-	google.golang.org/api v0.3.2 // indirect
+	google.golang.org/api v0.3.2
 )
--- data/upload.go
+++ data/upload.go
@@ -1,81 +1,175 @@
 package main
 
 import (
-    "bufio"
-    "encoding/csv"
-    "encoding/json"
-    "fmt"
-    "io"
-    "log"
-		"os"
-		"time"
-		"strconv"
+	"bufio"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"time"
 
-		firebase "firebase.google.com/go"
-		"golang.org/x/net/context"
-		"google.golang.org/api/option"
+	firebase "firebase.google.com/go"
+	"golang.org/x/net/context"
+	"google.golang.org/api/option"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 type Website struct {
-    Date string   `json:"date"`
-    Name  string   `json:"name"`
-    Visits   int `json:"visits"`
+	Date   string `json:"date"`
+	Name   string `json:"name"`
+	Visits int    `json:"visits"`
 }
 
-
-
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("error")
+	}
+	filename := os.Args[1]
+	err := waitUntilFind(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer watcher.Close()
+
+	err = watcher.Add(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	uploadCh := make(chan bool)
+	// removeCh := make(chan bool)
+	errCh := make(chan error)
+
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				switch {
+				case event.Op&fsnotify.Write == fsnotify.Write:
+					log.Printf("Write:  %s: %s", event.Op, event.Name)
+					uploadCh <- true
+					// case event.Op&fsnotify.Create == fsnotify.Create:
+					// 	log.Printf("Create: %s: %s", event.Op, event.Name)
+					// case event.Op&fsnotify.Remove == fsnotify.Remove:
+					// 	log.Printf("Remove: %s: %s", event.Op, event.Name)
+					// 	removeCh <- true
+					// case event.Op&fsnotify.Rename == fsnotify.Rename:
+					// 	log.Printf("Rename: %s: %s", event.Op, event.Name)
+					// 	renameCh <- true
+					// case event.Op&fsnotify.Chmod == fsnotify.Chmod:
+					// 	log.Printf("Chmod:  %s: %s", event.Op, event.Name)
+				}
+			case err := <-watcher.Errors:
+				errCh <- err
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-uploadCh:
+				err = waitUntilFind(filename)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				err = watcher.Add(filename)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				upload(filename)
+				// case <-removeCh:
+				// 	err = waitUntilFind(filename)
+				// 	if err != nil {
+				// 		log.Fatalln(err)
+				// 	}
+				// 	err = watcher.Add(filename)
+				// 	if err != nil {
+				// 		log.Fatalln(err)
+				// 	}
+			}
+		}
+	}()
+
+	log.Fatalln(<-errCh)
+}
+
+func upload(filename string) {
 	var err error
-    csvFile, _ := os.Open("data.csv")
-		reader := csv.NewReader(bufio.NewReader(csvFile))
-		reader.Comma = '|'
-    var websites []Website
-    for {
-        line, error := reader.Read()
-        if error == io.EOF {
-            break
-        } else if error != nil {
-            log.Fatal(error)
-				}
-				var visit int
-				visit, err = strconv.Atoi(line[2])
-				if err !=nil {
-					fmt.Printf("Cannot evaluate #%v\n", line)
-					continue;
-				}
-				var dt time.Time
-				dt, err =  time.Parse("2006-01-02", line[0])
-				if err !=nil {
-					fmt.Printf("Cannot evaluate #%v\n", line)
-					continue;
-				}//2016-01-06T00:00:00.000Z
-        websites = append(websites, Website{
-            Date: dt.Format("2006-01-02T15:04:05.000Z"),
-            Name:  line[1],
-            Visits: visit,
-        })
-    }
-    websiteJson, _ := json.Marshal(websites)
-		fmt.Println(string(websiteJson))
-		
-		// firebase
-		ctx := context.Background()
-		config := &firebase.Config{
-			DatabaseURL: "https://challenge-m-backend.firebaseio.com",
+	csvFile, _ := os.Open(filename)
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	reader.Comma = '|'
+	var websites []Website
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
 		}
-		opt := option.WithCredentialsFile("./challenge-m-backend-cacde0f5638f.json")
-		app, err := firebase.NewApp(ctx, config, opt)
+		var visit int
+		visit, err = strconv.Atoi(line[2])
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("Cannot evaluate #%v\n", line)
+			continue
 		}
-		
-		client, err := app.Database(ctx)
+		var dt time.Time
+		dt, err = time.Parse("2006-01-02", line[0])
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("Cannot evaluate #%v\n", line)
+			continue
+		} //2016-01-06T00:00:00.000Z
+		websites = append(websites, Website{
+			Date:   dt.Format("2006-01-02T15:04:05.000Z"),
+			Name:   line[1],
+			Visits: visit,
+		})
+	}
+	websiteJson, _ := json.Marshal(websites)
+	fmt.Println(string(websiteJson))
+
+	// firebase
+	ctx := context.Background()
+	config := &firebase.Config{
+		DatabaseURL: "https://challenge-m-backend.firebaseio.com",
+	}
+	opt := option.WithCredentialsFile("./challenge-m-backend-cacde0f5638f.json")
+	app, err := firebase.NewApp(ctx, config, opt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := app.Database(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.NewRef("websites").Set(ctx, websites); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func waitUntilFind(filename string) error {
+	for {
+		time.Sleep(1 * time.Second)
+		_, err := os.Stat(filename)
+		if err != nil {
+			if os.IsNotExist(err) {
+				continue
+			} else {
+				return err
+			}
 		}
-		
-	
-		if err := client.NewRef("websites").Set(ctx, websites); err != nil {
-			log.Fatal(err)
-		}
+		break
+	}
+	return nil
 }
